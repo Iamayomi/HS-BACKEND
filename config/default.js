f96c7b7095217223472a93b3ad7e855e7c13a7cf
Amodu Ayomide <77242644+Iamayomi@users.noreply.github.com>
2024-11-18 12:31:40 +0100

    adjust database connection

--- config/default.js
+++ config/default.js
@@ -37,9 +37,7 @@ module.exports = {
   // 'sendgridApiKey': process.env.SENDGRIDKEY,
   // 'emailLinkHostPrefix': 'http://localhost:3030',
 
-  // 'mongodb': "mongodb+srv://ayomidesherif2019:nr7RCjn36amnD5jC@cluster0.tpfcc.mongodb.net/hs-backend?retryWrites=true&w=majority&appName=Cluster0",
-
-  'mongodb': "mongodb+srv://ayomidesherif2019:nr7RCjn36amnD5jC@cluster0.tpfcc.mongodb.net/hs-backend",
+  'mongodb': "mongodb+srv://ayomidesherif2019:nr7RCjn36amnD5jC@cluster0.tpfcc.mongodb.net/hs-backend?retryWrites=true&w=majority&appName=Cluster0",
   'accessKeyId': process.env.ACCESSKEYID,
   'secretAccessKey': process.env.ACCESSSECRETKEY,
   's3Bucket': process.env.BUCKET,
